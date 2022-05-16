b2147f2b67add3298b27daf4cd1cb2e232448e98
Agustin Leguizamon <agustin.leg7@gmail.com>
2022-05-16 11:33:35 -0300

    🪄 ready

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,27 @@
+import HalfStar from "../../assets/half-star.png";
+import FullStar from "../../assets/full-star.png";
+import "./Card.css";
+
+const Card = ({ button }) => {
+  const totalStars = [1, 2, 3, 4, 5];
+
+  return (
+    <div className="card__container">
+      <h2 className="card__title">{button.title}</h2>
+      <p className="card_description">{button.description}</p>
+      <div className="card__stars">
+        {totalStars.map((star) => (
+          <img
+            key={star}
+            src={star <= button.stars ? FullStar : HalfStar}
+            alt="star"
+            width="25px"
+          />
+        ))}
+      </div>
+      <button style={button.style}>Boton</button>
+    </div>
+  );
+};
+
+export { Card };
